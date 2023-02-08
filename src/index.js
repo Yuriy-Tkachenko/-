458574b4f1756fabf1c76c2ca84995155847694e
Yuriy-Tkachenko <apolo16@inbox.ru>
2023-02-08 20:46:30 +0300

    created page theme and added dependencies

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/app/app";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <App />,
+  rootElement
+);
---
+++ src/thems/default.js
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  blacktColor: "#414141",
+  orangeColor: "#FF6633",
+  decorGreenColor: "#70C05B",
+  decorGreyColor: "#F3F2F1",
+  backgroundColor: "#FBF8EC",
+  backgroundOfferOrangeColor: "#FCD5BA",
+  backgroundOfferGreenColor: "#E5FFDE",
+  indent: "40px",
+  fornFamily: '"Rubik", "Arial", sans-serif',
+  fontSizeDefault: "12px",
+  fontWeightDefault: "400",
+  pageWidth: "1208px",
+  footerHeight: "126px",
+}
